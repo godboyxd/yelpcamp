e36efb435b0faf488298804a135be335c9b178f7
Henry Jackson <henry.jackson.95@gmail.com>
2018-04-09 23:54:25 +0000

    add DATABASEURL

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ var commentRoutes   = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes     = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://henry:mypassword@ds241039.mlab.com:41039/yelpcamp");
-// mongodb://henry:mypassword@ds241039.mlab.com:41039/yelpcamp
+mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
